93217071227c212b752ca790f212445729276c0c
cvr-devops <venkat.chittipeddi01@gmail.com>
2025-07-03 16:08:01 +0530

    cicd revised

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello welcome to cicd-practice!" );
+        System.out.println( "Hello praveenkumar welcome to cicd!" );
     }
 }
