f59b842133825e086c2d96395696e4e03d3d978c
cvr-devops <venkat.chittipeddi01@gmail.com>
2025-07-03 16:15:52 +0530

    great job

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello praveenkumar welcome to cicd!" );
+        System.out.println( "Hello praveenkumar welcome to cicd! great job!" );
     }
 }
