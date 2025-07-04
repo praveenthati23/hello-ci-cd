400dfaff177f2d2b5eb89d0bb7425868aad2bafe
cvr-devops <venkat.chittipeddi01@gmail.com>
2025-07-04 12:11:18 +0530

    added bugs in each tool

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,14 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello praveenkumar welcome to cicd! great job!" );
+public class App {
+
+    private static String unusedField = "I'm not used"; // PMD: UnusedPrivateField
+
+    public static void main(String[] args) {
+        Object obj = null;
+        System.out.println(obj.toString()); // SpotBugs: NP_NULL_ON_SOME_PATH
+
+        int a=5;int b=10;System.out.println(a+b); // Checkstyle: Multiple statements on one line
     }
 }
+
